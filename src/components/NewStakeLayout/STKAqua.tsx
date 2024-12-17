d21cddae969781c86d3dc43561b709547d2f5bfb
Samuel Anthony <60751900+thellecodes@users.noreply.github.com>
2024-12-17 17:37:45 +0300

    working on functionalities

--- src/components/NewStakeLayout/STKAqua.tsx
+++ src/components/NewStakeLayout/STKAqua.tsx
@@ -1,8 +1,245 @@
 import AquaLogo from "../../assets/images/aqua_logo.png";
 import { Button, Input } from "@headlessui/react";
 import clsx from "clsx";
+import { useAppDispatch } from "../../lib/hooks";
+import { useSelector } from "react-redux";
+import { RootState } from "../../lib/store";
+import { useEffect, useState } from "react";
+import {
+  FREIGHTER_ID,
+  FreighterModule,
+  LOBSTR_ID,
+  LobstrModule,
+  StellarWalletsKit,
+  WalletNetwork,
+} from "@creit.tech/stellar-wallets-kit";
+import { StellarService } from "../../services/stellar.service";
+import {
+  getAccountInfo,
+  lockingAqua,
+  mint,
+  resetStateValues,
+  storeAccountBalance,
+} from "../../lib/slices/userSlice";
+import {
+  Asset,
+  BASE_FEE,
+  Networks,
+  Operation,
+  TransactionBuilder,
+} from "@stellar/stellar-sdk";
+import {
+  aquaAssetCode,
+  aquaAssetIssuer,
+  blubAssetCode,
+  blubIssuer,
+  blubSignerPublicKey,
+} from "../../utils/constants";
+import { toast } from "react-toastify";
+import { Balance } from "../../utils/interfaces";
+import { MIN_DEPOSIT_AMOUNT } from "../../config";
 
 function STKAqua() {
+  const dispatch = useAppDispatch();
+  const user = useSelector((state: RootState) => state.user);
+  const [aquaDepositAmount, setAquaDepositAmount] = useState<number | null>();
+
+  //get user aqua record
+  const aquaRecord = user?.userRecords?.balances?.find(
+    (balance) => balance.asset_code === "AQUA"
+  );
+
+  const userAquaBalance = aquaRecord?.balance;
+
+  const updateWalletRecords = async () => {
+    const selectedModule =
+      user?.walletName === LOBSTR_ID
+        ? new LobstrModule()
+        : new FreighterModule();
+
+    const kit: StellarWalletsKit = new StellarWalletsKit({
+      network: WalletNetwork.PUBLIC,
+      selectedWalletId: FREIGHTER_ID,
+      modules: [selectedModule],
+    });
+
+    const { address } = await kit.getAddress();
+    const stellarService = new StellarService();
+    const wrappedAccount = await stellarService.loadAccount(address);
+
+    dispatch(getAccountInfo(address));
+    dispatch(storeAccountBalance(wrappedAccount.balances));
+  };
+
+  const handleSetMaxDeposit = () => {
+    setAquaDepositAmount(Number(userAquaBalance));
+  };
+
+  const handleAddTrustline = async () => {
+    const selectedModule =
+      user?.walletName === LOBSTR_ID
+        ? new LobstrModule()
+        : new FreighterModule();
+
+    const kit = new StellarWalletsKit({
+      network: WalletNetwork.PUBLIC,
+      selectedWalletId:
+        user?.walletName === LOBSTR_ID ? LOBSTR_ID : FREIGHTER_ID,
+      modules: [selectedModule],
+    });
+
+    const stellarService = new StellarService();
+    const { address } = await kit.getAddress();
+    const senderAccount = await stellarService.loadAccount(address);
+
+    const transactionBuilder = new TransactionBuilder(senderAccount, {
+      fee: BASE_FEE,
+      networkPassphrase: Networks.PUBLIC,
+    });
+
+    const trustlineOperation = Operation.changeTrust({
+      asset: new Asset(blubAssetCode, blubIssuer),
+      limit: "1000000000",
+    });
+
+    const transactionXDR = transactionBuilder
+      .addOperation(trustlineOperation)
+      .setTimeout(30)
+      .build()
+      .toXDR();
+
+    const { signedTxXdr } = await kit.signTransaction(transactionXDR, {
+      address,
+      networkPassphrase: WalletNetwork.PUBLIC,
+    });
+
+    const HORIZON_SERVER = "https://horizon.stellar.org";
+
+    const transactionToSubmit = TransactionBuilder.fromXDR(
+      signedTxXdr,
+      HORIZON_SERVER
+    );
+
+    await stellarService?.server?.submitTransaction(transactionToSubmit);
+  };
+
+  const handleLockAqua = async () => {
+    const selectedModule =
+      user?.walletName === LOBSTR_ID
+        ? new LobstrModule()
+        : new FreighterModule();
+
+    const kit: StellarWalletsKit = new StellarWalletsKit({
+      network: WalletNetwork.PUBLIC,
+      selectedWalletId:
+        user?.walletName === LOBSTR_ID ? LOBSTR_ID : FREIGHTER_ID,
+      modules: [selectedModule],
+    });
+
+    dispatch(lockingAqua(true));
+
+    const stellarService = new StellarService();
+    const { address } = await kit.getAddress();
+
+    if (!address) {
+      dispatch(lockingAqua(false));
+      return toast.warn("Please connect wallet.");
+    }
+
+    if (!user) {
+      dispatch(lockingAqua(false));
+      return toast.warn("Global state not initialized.");
+    }
+
+    if (!aquaDepositAmount) {
+      dispatch(lockingAqua(false));
+      return toast.warn("Please input amount to stake.");
+    }
+
+    if (aquaDepositAmount < MIN_DEPOSIT_AMOUNT) {
+      dispatch(lockingAqua(false));
+      return toast.warn(
+        `Deposit amount should be higher than ${MIN_DEPOSIT_AMOUNT}.`
+      );
+    }
+
+    const senderAccount = await stellarService.loadAccount(address);
+    const existingTrustlines = senderAccount.balances.map(
+      (balance: Balance) => balance.asset_code
+    );
+
+    if (!existingTrustlines.includes(blubAssetCode)) {
+      try {
+        await handleAddTrustline();
+        toast.success("Trustline added successfully.");
+      } catch (error) {
+        dispatch(lockingAqua(false));
+        return toast.error("Failed to add trustline.");
+      }
+    }
+
+    try {
+      const customAsset = new Asset(aquaAssetCode, aquaAssetIssuer);
+      const stakeAmount = aquaDepositAmount.toFixed(7);
+
+      const paymentOperation = Operation.payment({
+        destination: blubSignerPublicKey,
+        asset: customAsset,
+        amount: stakeAmount,
+      });
+
+      const transactionBuilder = new TransactionBuilder(senderAccount, {
+        fee: BASE_FEE,
+        networkPassphrase: Networks.PUBLIC,
+      });
+
+      transactionBuilder.addOperation(paymentOperation).setTimeout(180);
+
+      const transaction = transactionBuilder.build();
+
+      const transactionXDR = transaction.toXDR();
+
+      const { signedTxXdr } = await kit.signTransaction(transactionXDR, {
+        address,
+        networkPassphrase: WalletNetwork.PUBLIC,
+      });
+
+      dispatch(
+        mint({
+          assetCode: aquaAssetCode,
+          assetIssuer: aquaAssetIssuer,
+          amount: stakeAmount,
+          signedTxXdr,
+          senderPublicKey: address,
+        })
+      );
+
+      dispatch(lockingAqua(true));
+      toast.success("Transaction sent!");
+    } catch (err) {
+      console.error("Transaction failed:", err);
+      dispatch(lockingAqua(false));
+    }
+  };
+
+  useEffect(() => {
+    if (user?.lockedAqua) {
+      updateWalletRecords();
+      toast.success("Aqua locked successfully!");
+      setAquaDepositAmount(0);
+      dispatch(lockingAqua(false));
+      dispatch(resetStateValues());
+    }
+
+    if (user?.lockedAqua) {
+      updateWalletRecords();
+      toast.success("Aqua locked successfully!");
+      setAquaDepositAmount(0);
+      dispatch(lockingAqua(false));
+      dispatch(resetStateValues());
+    }
+  }, [user?.lockedAqua, user?.lockedAqua]);
+
   return (
     <div>
       <div className="mx-auto">
@@ -41,7 +278,7 @@ function STKAqua() {
               <i className="fa fa-exclamation-circle" aria-hidden="true"></i>
             </div>
 
-            <div className="flex items-center bg-[#0E111B] px-5 py-2 space-x-2 mt-2 rounded-[8px]">
+            <div className="flex items-center bg-[#0E111B]  py-2 space-x-2 mt-2 rounded-[8px]">
               <Input
                 placeholder="0 AQUA"
                 className={clsx(
@@ -49,8 +286,18 @@ function STKAqua() {
                   "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-[#3C404D]",
                   "w-full p-3 bg-none"
                 )}
+                onChange={(e) =>
+                  setAquaDepositAmount(
+                    e.target.value ? Number(e.target.value) : null
+                  )
+                }
               />
-              <button className="bg-[#3C404D] p-2 rounded-[4px]">Max</button>
+              <button
+                className="bg-[#3C404D] p-2 rounded-[4px]"
+                onClick={handleSetMaxDeposit}
+              >
+                Max
+              </button>
             </div>
 
             <div className="flex items-center text-normal mt-6 space-x-1">
@@ -58,7 +305,11 @@ function STKAqua() {
               <div className="font-medium">0 BLUB</div>
             </div>
 
-            <Button className="rounded-[12px] py-5 px-4 text-white mt-10 w-full bg-[linear-gradient(180deg,_#00CC99_0%,_#005F99_100%)] text-base font-semibold">
+            <Button
+              className="rounded-[12px] py-5 px-4 text-white mt-10 w-full bg-[linear-gradient(180deg,_#00CC99_0%,_#005F99_100%)] text-base font-semibold"
+              onClick={handleLockAqua}
+              disabled={user?.lockingAqua}
+            >
               Convert & Stake
             </Button>
           </div>
@@ -88,7 +339,9 @@ function STKAqua() {
 
             <div className="flex items-center bg-[#0E111B] px-5 py-2 mt-5 rounded-[8px] justify-between">
               <div className="text-sm font-normal text-white">Total</div>
-              <div className="p-2 text-2xl font-normal">0 BLUB</div>
+              <div className="p-2 text-2xl font-normal">
+                {user?.userLockedRewardsAmount ?? 0} BLUB
+              </div>
             </div>
           </div>
         </div>
--- src/components/NewStakeLayout/Yeild.tsx
+++ src/components/NewStakeLayout/Yeild.tsx
@@ -1,7 +1,251 @@
 import clsx from "clsx";
 import { Button, Input } from "@headlessui/react";
+import { useEffect, useState } from "react";
+import { useAppDispatch } from "../../lib/hooks";
+import { useSelector } from "react-redux";
+import { RootState } from "../../lib/store";
+import {
+  FREIGHTER_ID,
+  FreighterModule,
+  LOBSTR_ID,
+  LobstrModule,
+  StellarWalletsKit,
+  WalletNetwork,
+} from "@creit.tech/stellar-wallets-kit";
+import { toast } from "react-toastify";
+import {
+  getAccountInfo,
+  lockingAqua,
+  resetStateValues,
+  restakeBlub,
+  restaking,
+  storeAccountBalance,
+  unStakeAqua,
+  unStakingAqua,
+} from "../../lib/slices/userSlice";
+import { StellarService } from "../../services/stellar.service";
+import { Balance } from "../../utils/interfaces";
+import {
+  blubAssetCode,
+  blubIssuer,
+  blubIssuerPublicKey,
+  lpSignerPublicKey,
+} from "../../utils/constants";
+import {
+  Asset,
+  BASE_FEE,
+  Networks,
+  Operation,
+  TransactionBuilder,
+} from "@stellar/stellar-sdk";
+import { TailSpin } from "react-loader-spinner";
+import AddLiquidity from "./AddLiquidity";
 
 function Yeild() {
+  const [isBlubStakeExpanded, setIsBlubStakeExpanded] =
+    useState<boolean>(false);
+  const [blubStakeAmount, setBlubStakeAmount] = useState<number | null>(0);
+  const dispatch = useAppDispatch();
+  const [blubUnstakeAmount, setBlubUnstakeAmount] = useState<number | null>(0);
+
+  const user = useSelector((state: RootState) => state.user);
+
+  const whlAquaRecord = user?.userRecords?.balances?.find(
+    (balance) => balance.asset_code === "WHLAQUA"
+  );
+
+  //get user aqua record
+  const aquaRecord = user?.userRecords?.balances?.find(
+    (balance) => balance.asset_code === "AQUA"
+  );
+
+  const userAquaBalance = aquaRecord?.balance;
+  // const whlAquaBalance = whlAquaRecord?.balance;
+  const blubBalance = whlAquaRecord?.balance;
+
+  // Calculate accountClaimableRecords
+  const accountClaimableRecords =
+    user?.userRecords?.account?.claimableRecords
+      ?.filter((record: any) => record.claimed === "UNCLAIMED")
+      ?.reduce((total, record: any) => {
+        return Number(total) + Number(record.amount);
+      }, 0) || 0;
+
+  const userPoolBalances =
+    user?.userRecords?.account?.pools
+      ?.filter((pool: any) => pool.claimed === "UNCLAIMED")
+      ?.filter((pool: any) => pool.depositType === "LOCKER")
+      ?.filter((pool: any) => pool.assetB.code === "AQUA")
+      ?.reduce((total, record: any) => {
+        return Number(total) + Number(record.assetBAmount);
+      }, 0) || 0;
+
+  // Add the two calculated values
+  const poolAndClaimBalance =
+    Number(userPoolBalances) + Number(accountClaimableRecords);
+
+  const handleSetMaxDepositForBlub = () => {
+    setBlubUnstakeAmount(Number(accountClaimableRecords));
+  };
+
+  const handleUnstakeAqua = async () => {
+    if (poolAndClaimBalance < 1 || Number(blubUnstakeAmount) < 1)
+      return toast.warn("Nothing to unstake");
+
+    if (Number(blubUnstakeAmount) > poolAndClaimBalance)
+      return toast.warn("Unstake amount exceeds the pool balance");
+
+    const selectedModule =
+      user?.walletName === LOBSTR_ID
+        ? new LobstrModule()
+        : new FreighterModule();
+
+    const kit: StellarWalletsKit = new StellarWalletsKit({
+      network: WalletNetwork.PUBLIC,
+      selectedWalletId:
+        user?.walletName === LOBSTR_ID ? LOBSTR_ID : FREIGHTER_ID,
+      modules: [selectedModule],
+    });
+
+    const { address } = await kit.getAddress();
+    dispatch(unStakingAqua(true));
+    dispatch(
+      unStakeAqua({
+        senderPublicKey: address,
+        amountToUnstake: Number(blubUnstakeAmount),
+      })
+    );
+  };
+
+  const handleSetRestakeMaxDeposit = () => {
+    setBlubStakeAmount(Number(blubBalance));
+  };
+
+  const updateWalletRecords = async () => {
+    const selectedModule =
+      user?.walletName === LOBSTR_ID
+        ? new LobstrModule()
+        : new FreighterModule();
+
+    const kit: StellarWalletsKit = new StellarWalletsKit({
+      network: WalletNetwork.PUBLIC,
+      selectedWalletId: FREIGHTER_ID,
+      modules: [selectedModule],
+    });
+
+    const { address } = await kit.getAddress();
+    const stellarService = new StellarService();
+    const wrappedAccount = await stellarService.loadAccount(address);
+
+    dispatch(getAccountInfo(address));
+    dispatch(storeAccountBalance(wrappedAccount.balances));
+  };
+
+  const handleRestake = async () => {
+    const selectedModule =
+      user?.walletName === LOBSTR_ID
+        ? new LobstrModule()
+        : new FreighterModule();
+
+    const kit: StellarWalletsKit = new StellarWalletsKit({
+      network: WalletNetwork.PUBLIC,
+      selectedWalletId:
+        user?.walletName === LOBSTR_ID ? LOBSTR_ID : FREIGHTER_ID,
+      modules: [selectedModule],
+    });
+
+    const { address } = await kit.getAddress();
+
+    if (!address) {
+      dispatch(lockingAqua(false));
+      return toast.warn("Please connect wallet.");
+    }
+
+    if (!user) {
+      dispatch(lockingAqua(false));
+      return toast.warn("Global state not initialized.");
+    }
+
+    if (!blubStakeAmount) {
+      dispatch(lockingAqua(false));
+      return toast.warn("Please input amount to stake.");
+    }
+
+    if (Number(blubBalance) < blubStakeAmount) {
+      dispatch(lockingAqua(false));
+      return toast.warn(`Your balance is low`);
+    }
+
+    dispatch(restaking(true));
+    const stellarService = new StellarService();
+    const senderAccount = await stellarService.loadAccount(address);
+
+    const existingTrustlines = senderAccount.balances.map(
+      (balance: Balance) => balance.asset_code
+    );
+
+    if (!existingTrustlines.includes(blubAssetCode)) {
+      dispatch(restaking(false));
+      return toast.warn(`You need trustline for ${blubAssetCode}`);
+    }
+
+    try {
+      const stakeAmount = blubStakeAmount.toFixed(7);
+
+      const paymentOperation = Operation.payment({
+        destination: lpSignerPublicKey,
+        asset: new Asset(blubAssetCode, blubIssuer),
+        amount: stakeAmount,
+      });
+
+      const transactionBuilder = new TransactionBuilder(senderAccount, {
+        fee: BASE_FEE,
+        networkPassphrase: Networks.PUBLIC,
+      });
+
+      transactionBuilder.addOperation(paymentOperation).setTimeout(180);
+
+      const transaction = transactionBuilder.build();
+
+      const transactionXDR = transaction.toXDR();
+
+      const { signedTxXdr } = await kit.signTransaction(transactionXDR, {
+        address,
+        networkPassphrase: WalletNetwork.PUBLIC,
+      });
+
+      dispatch(
+        restakeBlub({
+          assetCode: "WHLAQUA",
+          assetIssuer: blubIssuerPublicKey,
+          amount: `${blubStakeAmount}`,
+          signedTxXdr,
+          senderPublicKey: address,
+        })
+      );
+      dispatch(restaking(true));
+    } catch (err) {
+      console.log(err);
+      dispatch(restaking(false));
+    }
+  };
+
+  useEffect(() => {
+    if (user?.restaked) {
+      updateWalletRecords();
+      toast.success("BLUB Locked successfully!");
+      dispatch(resetStateValues());
+      dispatch(restaking(false));
+    }
+
+    if (user?.unStakedAqua) {
+      updateWalletRecords();
+      toast.success("Blub unstaked successfully!");
+      dispatch(resetStateValues());
+      dispatch(unStakingAqua(false));
+    }
+  }, [user?.restaked, user?.unStakedAqua]);
+
   return (
     <div>
       <div className="max-w-[912px] mx-auto">
@@ -40,6 +284,13 @@ function Yeild() {
                     "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-[#3C404D]",
                     "w-full p-3 bg-none"
                   )}
+                  value={blubStakeAmount != null ? blubStakeAmount : ""}
+                  onClick={handleSetMaxDepositForBlub}
+                  onChange={(e) =>
+                    setBlubStakeAmount(
+                      e.target.value ? Number(e.target.value) : null
+                    )
+                  }
                 />
                 <button className="bg-[#3C404D] p-2 rounded-[4px]">Max</button>
               </div>
@@ -48,11 +299,36 @@ function Yeild() {
                 <div className="font-normal text-[#B1B3B8]">
                   Staked Balance:
                 </div>
-                <div className="font-medium">0 BLUB</div>
+                <div className="font-medium">
+                  {`${
+                    isNaN(Number(blubBalance))
+                      ? 0
+                      : Number(blubBalance).toFixed(2)
+                  } BLUB`}
+                </div>
               </div>
 
-              <Button className="rounded-[12px] py-5 px-4 text-white mt-10 w-full bg-[linear-gradient(180deg,_#00CC99_0%,_#005F99_100%)] text-base font-semibold">
-                Unstake
+              <Button
+                className="rounded-[12px] py-5 px-4 text-white mt-10 w-full bg-[linear-gradient(180deg,_#00CC99_0%,_#005F99_100%)] text-base font-semibold"
+                onClick={handleRestake}
+              >
+                {!user?.restaking ? (
+                  <span>Stake </span>
+                ) : (
+                  <div className="flex justify-center items-center gap-[10px]">
+                    <span className="text-white">Processing...</span>
+                    <TailSpin
+                      height="18"
+                      width="18"
+                      color="#ffffff"
+                      ariaLabel="tail-spin-loading"
+                      radius="1"
+                      wrapperStyle={{}}
+                      wrapperClass=""
+                      visible={true}
+                    />
+                  </div>
+                )}
               </Button>
             </div>
 
@@ -61,14 +337,24 @@ function Yeild() {
             <div>
               <div className="flex items-center bg-[#0E111B] px-5 py-2 space-x-2 mt-2 rounded-[8px]">
                 <Input
-                  placeholder="0 AQUA"
+                  placeholder="0 BLUB"
                   className={clsx(
                     "block w-full rounded-lg border-none bg-[#0E111B] px-3 text-sm/6 text-white",
                     "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-[#3C404D]",
                     "w-full p-3 bg-none"
                   )}
+                  onChange={(e) =>
+                    setBlubUnstakeAmount(
+                      e.target.value ? Number(e.target.value) : null
+                    )
+                  }
                 />
-                <button className="bg-[#3C404D] p-2 rounded-[4px]">Max</button>
+                <button
+                  className="bg-[#3C404D] p-2 rounded-[4px]"
+                  onClick={handleSetMaxDepositForBlub}
+                >
+                  Max
+                </button>
               </div>
 
               <div className="flex items-center text-normal mt-6 space-x-1">
@@ -78,66 +364,32 @@ function Yeild() {
                 <div className="font-medium">0 BLUB</div>
               </div>
 
-              <Button className="rounded-[12px] py-5 px-4 text-white mt-10 w-full bg-[linear-gradient(180deg,_#00CC99_0%,_#005F99_100%)] text-base font-semibold">
-                Unstake
+              <Button
+                className="rounded-[12px] py-5 px-4 text-white mt-10 w-full bg-[linear-gradient(180deg,_#00CC99_0%,_#005F99_100%)] text-base font-semibold"
+                onClick={handleUnstakeAqua}
+              >
+                {!user?.unStakingAqua ? (
+                  <span>Unstake</span>
+                ) : (
+                  <div className="flex justify-center items-center gap-[10px]">
+                    <span className="text-white">Processing...</span>
+                    <TailSpin
+                      height="18"
+                      width="18"
+                      color="#ffffff"
+                      ariaLabel="tail-spin-loading"
+                      radius="1"
+                      wrapperStyle={{}}
+                      wrapperClass=""
+                      visible={true}
+                    />
+                  </div>
+                )}
               </Button>
             </div>
           </div>
         </div>
-        <div>
-          <div className="bg-[#0E111BCC] p-10 rounded-[16px]">
-            <div className="flex items-center space-x-4">
-              <div className="flex items-center space-x-2">
-                <img
-                  src={"/Blub_logo2.svg"}
-                  alt="Aqua"
-                  className="w-8 h-8 rounded-full"
-                />
-                <span className="text-lg">BLUB</span>
-              </div>
-            </div>
-            <div className="text-2xl font-medium text-white mt-5 flex items-center space-x-2">
-              <div>Boost liquidity pool for yield</div>
-              <i className="fa fa-exclamation-circle" aria-hidden="true"></i>
-            </div>
-
-            <div className="flex items-center bg-[#0E111B] px-5 py-2 space-x-2 mt-2 rounded-[8px]">
-              <Input
-                placeholder="0 AQUA"
-                className={clsx(
-                  "block w-full rounded-lg border-none bg-[#0E111B] px-3 text-sm/6 text-white",
-                  "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-[#3C404D]",
-                  "w-full p-3 bg-none"
-                )}
-              />
-              <button className="bg-[#3C404D] p-2 rounded-[4px]">Max</button>
-            </div>
-            <div className="flex items-center text-normal mt-2 space-x-1">
-              <div className="font-normal text-[#B1B3B8]">Balance:</div>
-              <div className="font-medium">0 BLUB</div>
-            </div>
-
-            <div className="flex items-center bg-[#0E111B] px-5 py-2 space-x-2 mt-5 rounded-[8px]">
-              <Input
-                placeholder="0 AQUA"
-                className={clsx(
-                  "block w-full rounded-lg border-none bg-[#0E111B] px-3 text-sm/6 text-white",
-                  "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-[#3C404D]",
-                  "w-full p-3 bg-none"
-                )}
-              />
-              <button className="bg-[#3C404D] p-2 rounded-[4px]">Max</button>
-            </div>
-            <div className="flex items-center text-normal mt-2 space-x-1">
-              <div className="font-normal text-[#B1B3B8]">Balance:</div>
-              <div className="font-medium">0 BLUB</div>
-            </div>
-
-            <Button className="rounded-[12px] py-5 px-4 text-white mt-10 w-full bg-[linear-gradient(180deg,_#00CC99_0%,_#005F99_100%)] text-base font-semibold">
-              Lock for Yeild
-            </Button>
-          </div>
-        </div>
+        <AddLiquidity />
       </div>
     </div>
   );
