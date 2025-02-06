5e3d2891ff0f600a5a5dab7a02b76fefc43e54b4
Viktor <99098789+combased-dev@users.noreply.github.com>
2025-02-06 18:49:17 +0200

    cganged

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,12 +1,12 @@
-export const sorobanMainnetRpcEndpoint: string = `https://mainnet.stellar.validationcloud.io/v1/r39vq61bkyEQIpOaurlWjJYr77HrcayUC_4sblcB_K8`;
+export const sorobanMainnetRpcEndpoint: string = `https://mainnet.stellar.validationcloud.io/v1/-P7RQ40r_IxrDAC-yAEQC8EbNpgDYNyvzm2-wK5wWVQ`;
 export const blubIssuerPublicKey: string =
-  "GCX6LOZ6ZEXBHLTPOPP2THN74K33LMT4HKSPDTWSLVCF4EWRGXOS7D3V";
+  "GCYITYIOHQIDYK5ASX5CC7LVC4WOTHMCMPNDCTPARUUFHYDV2EBX55IQ";
 export const blubSignerPublicKey: string =
-  "GDJ2BJCYLWFCLDVF4THQLQDTTQGBJ4UJ3OQTJ3IHJ5L3E2ZTLSCIGOSH";
+  "GCYITYIOHQIDYK5ASX5CC7LVC4WOTHMCMPNDCTPARUUFHYDV2EBX55IQ";
 export const lpSignerPublicKey: string =
-  "GDJ2BJCYLWFCLDVF4THQLQDTTQGBJ4UJ3OQTJ3IHJ5L3E2ZTLSCIGOSH";
+  "GCYITYIOHQIDYK5ASX5CC7LVC4WOTHMCMPNDCTPARUUFHYDV2EBX55IQ";
 export const treasureAddress: string =
-  "GC6UELQOGTXPOKGIG52T5ZC42YGNZIZATMJF7A3HM3TGRFOC3WSPMWXA";
+  "GCKTMO57VPZIOMFW47ZHXNWARPQRO4UGNNLHVFSEKDB2XKC74ZP4EKXD";
 export const BACKEND_API = process.env.REACT_APP_BACKEND_URL;
 
 export const aquaAssetCode = "AQUA";
@@ -15,7 +15,7 @@ export const aquaAssetIssuer =
 
 export const blubAssetCode = "BLUB";
 export const blubIssuer =
-  "GCX6LOZ6ZEXBHLTPOPP2THN74K33LMT4HKSPDTWSLVCF4EWRGXOS7D3V";
+  "GCYITYIOHQIDYK5ASX5CC7LVC4WOTHMCMPNDCTPARUUFHYDV2EBX55IQ";
 
 export const usdcAssetCode = "USDC";
 export const usdcIssuer =
